97ae1a49e31d175a8d56990283f69b973d8e277a
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-11-16 20:09:05 +0330

    Updates

---
+++ precache-manifest.77e153f276d88eae450ff0bbb6113580.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26a6b6a5d6814304c3f5444b4b05688",
+    "url": "/Movies-IMDB-Github/index.html"
+  },
+  {
+    "revision": "9d5ebc603185b9c7c4e5",
+    "url": "/Movies-IMDB-Github/static/css/2.12795900.chunk.css"
+  },
+  {
+    "revision": "1dc2997a6b8e919985bf",
+    "url": "/Movies-IMDB-Github/static/css/main.9126d0aa.chunk.css"
+  },
+  {
+    "revision": "9d5ebc603185b9c7c4e5",
+    "url": "/Movies-IMDB-Github/static/js/2.3d06c038.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/Movies-IMDB-Github/static/js/2.3d06c038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc2997a6b8e919985bf",
+    "url": "/Movies-IMDB-Github/static/js/main.2b2a8c77.chunk.js"
+  },
+  {
+    "revision": "2300e75dd19286e805d2",
+    "url": "/Movies-IMDB-Github/static/js/runtime-main.0c90338b.js"
+  },
+  {
+    "revision": "c22dde0bf07660166d4c1fbc7c26cd50",
+    "url": "/Movies-IMDB-Github/static/media/Header-Movies-Image.c22dde0b.jpg"
+  }
+]);
